376590612b89ba7692a6a4ada9508bf3548ecd75
Linh Vu <thuylinh2490@gmail.com>
2021-01-30 12:50:20 -0500

    feat(test): handle click button

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer')
 
 describe('My first puppeteer test',()=>{
-    it('should load the browser', async ()=>{
+    it('should see what is typing in the input field', async ()=>{
       const browser = await puppeteer.launch({
         headless: false,
         slowMo: 10,
@@ -9,8 +9,22 @@ describe('My first puppeteer test',()=>{
       })
       const page = await browser.newPage()
       await page.goto("https://www.amazon.com/")
-      await page.type("#twotabsearchtextbox", "cooker", {delay: 1000}) //1st argument is the selector, 2nd is value, 3rd arg-> slow motion -> slow typing speed -> dev to test
-      await page.waitFor(5000) // got to the browser for 5secs
+      await page.type("#twotabsearchtextbox", "cooker", {delay: 300}) //1st argument is the selector, 2nd is value, 3rd arg-> slow motion -> slow typing speed -> dev to test
+      await page.waitFor(2000) // got to the browser for 5secs
       await browser.close()
     })
+    it('should handle button', async ()=>{
+      const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 10,
+        devtools: false
+      })
+      const page = await browser.newPage()
+      await page.goto("https://www.amazon.com/")
+      await page.type("#twotabsearchtextbox", "cooker",{delay: 300})
+      await page.click("#nav-search-submit-button",{clickCount: 1})
+      await page.waitFor(5000)
+      await browser.close()
+    })
+
 })
